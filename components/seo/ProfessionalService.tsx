235802c8a83fcf68297ea71d60dd34eea1c2fead
saslandeco-cmyk <saslandeco@gmail.com>
2026-07-07 14:32:23 +0200

    Update ProfessionalService.tsx

--- components/seo/ProfessionalService.tsx
+++ components/seo/ProfessionalService.tsx
@@ -1,1 +1,32 @@
+export const professionalService = {
+  "@context": "https://schema.org",
+  "@type": "ProfessionalService",
 
+  "@id": "https://webmasterhautrhin.fr/#service",
+
+  name: "Webmaster Haut-Rhin",
+
+  url: "https://webmasterhautrhin.fr",
+
+  image: "https://webmasterhautrhin.fr/logo.png",
+
+  telephone: "+33663608012",
+
+  areaServed: {
+    "@type": "AdministrativeArea",
+    name: "Haut-Rhin",
+  },
+
+  description:
+    "Création de sites internet, référencement naturel SEO, maintenance WordPress et développement Next.js.",
+
+  serviceType: [
+    "Création de site internet",
+    "Référencement naturel",
+    "Développement Next.js",
+    "Maintenance WordPress",
+    "Hébergement web",
+  ],
+
+  priceRange: "€€",
+};
