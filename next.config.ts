2febe51371ecf4a9772cfe0129d204c01cc9a803
saslandeco-cmyk <saslandeco@gmail.com>
2026-07-07 10:12:08 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
+async redirects() {
+    return [
+      {
+        source: "/creation-de-site-internet-haut-rhin",
+        destination: "/creation-de-site-internet",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
