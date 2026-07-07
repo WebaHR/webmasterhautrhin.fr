fa5f28bccc7ac6e130f39b83f8cbedb55341efa4
saslandeco-cmyk <saslandeco@gmail.com>
2026-07-07 14:32:46 +0200

    Add LocalBusiness schema for Webmaster Haut-Rhin

--- components/seo/LocalBusiness.tsx
+++ components/seo/LocalBusiness.tsx
@@ -1,1 +1,28 @@
+export const localBusiness = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
 
+  "@id": "https://webmasterhautrhin.fr/#business",
+
+  name: "Webmaster Haut-Rhin",
+
+  url: "https://webmasterhautrhin.fr",
+
+  image: "https://webmasterhautrhin.fr/logo.png",
+
+  telephone: "+33663608012",
+
+  address: {
+    "@type": "PostalAddress",
+    addressRegion: "Grand Est",
+    addressLocality: "Mulhouse",
+    addressCountry: "FR",
+  },
+
+  areaServed: [
+    "Mulhouse",
+    "Colmar",
+    "Saint-Louis",
+    "Haut-Rhin",
+  ],
+};
