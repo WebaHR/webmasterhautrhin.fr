8edf741c368550edc3e20ed2ad8c187417a627bf
saslandeco-cmyk <saslandeco@gmail.com>
2026-07-07 10:59:19 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,30 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CookieConsent from "@/components/CookieConsent";
 
+<script
+type="application/ld+json"
+dangerouslySetInnerHTML={{
+__html: JSON.stringify({
+"@context": "https://schema.org",
+"@type": "ProfessionalService",
+"name": "Webmaster Haut-Rhin",
+"url": "https://webmasterhautrhin.fr",
+"description":
+"Création de sites internet, référencement SEO et maintenance dans le Haut-Rhin.",
+"areaServed": {
+"@type": "AdministrativeArea",
+"name": "Haut-Rhin"
+},
+"serviceType": [
+"Création de site internet",
+"SEO",
+"Hébergement web",
+"Maintenance"
+]
+})
+}}
+/>
+
 const display = Space_Grotesk({
   subsets: ["latin"],
   weight: ["500", "600", "700"],
