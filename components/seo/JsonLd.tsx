8e79d3dcb3fd2a1271b76bf82abe3b702c433d74
saslandeco-cmyk <saslandeco@gmail.com>
2026-07-07 14:34:14 +0200

    Update JsonLd.tsx

--- components/seo/JsonLd.tsx
+++ components/seo/JsonLd.tsx
@@ -1,1 +1,53 @@
+import Script from "next/script";
 
+import { professionalService } from "./ProfessionalService";
+import { localBusiness } from "./LocalBusiness";
+import { person } from "./Person";
+import { organization } from "./Organization";
+import { website } from "./Website";
+
+export default function JsonLd() {
+  return (
+    <>
+      <Script
+        id="schema-professional"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(professionalService),
+        }}
+      />
+
+      <Script
+        id="schema-business"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(localBusiness),
+        }}
+      />
+
+      <Script
+        id="schema-person"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(person),
+        }}
+      />
+
+      <Script
+        id="schema-organization"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(organization),
+        }}
+      />
+
+      <Script
+        id="schema-website"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(website),
+        }}
+      />
+    </>
+  );
+}
