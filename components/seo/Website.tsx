83feb06d549450f3a383688cb3888c485394bbe3
saslandeco-cmyk <saslandeco@gmail.com>
2026-07-07 14:33:38 +0200

    Update Website.tsx

--- components/seo/Website.tsx
+++ components/seo/Website.tsx
@@ -1,1 +1,22 @@
+export const website = {
+  "@context": "https://schema.org",
 
+  "@type": "WebSite",
+
+  "@id": "https://webmasterhautrhin.fr/#website",
+
+  url: "https://webmasterhautrhin.fr",
+
+  name: "Webmaster Haut-Rhin",
+
+  inLanguage: "fr",
+
+  potentialAction: {
+    "@type": "SearchAction",
+
+    target:
+      "https://webmasterhautrhin.fr/?s={search_term_string}",
+
+    "query-input": "required name=search_term_string",
+  },
+};
