43661d5c1dafbf0cab825866f5637a741c6fe76c
saslandeco-cmyk <saslandeco@gmail.com>
2026-07-07 14:33:12 +0200

    Update Person.tsx

--- components/seo/Person.tsx
+++ components/seo/Person.tsx
@@ -1,1 +1,24 @@
+export const person = {
+  "@context": "https://schema.org",
 
+  "@type": "Person",
+
+  name: "Sébastien",
+
+  jobTitle: "Webmaster",
+
+  worksFor: {
+    "@id": "https://webmasterhautrhin.fr/#business",
+  },
+
+  url: "https://webmasterhautrhin.fr",
+
+  knowsAbout: [
+    "SEO",
+    "Next.js",
+    "WordPress",
+    "React",
+    "TypeScript",
+    "Création de site internet",
+  ],
+};
